a19160a12702add51049342e2301f40c6d6bbfee
Rachel Zhou <rubynono2020@gmail.com>
2022-08-26 14:45:28 +1200

    added tic-tac-toe class and project files

---
+++ src/ticTacToeGameClass/GameState.java
@@ -0,0 +1,5 @@
+package ticTacToeGameClass;
+
+public enum GameState {
+	Playing, Draw, Cross_won, Nought_won
+}
---
+++ src/ticTacToeGameClass/Player.java
@@ -0,0 +1,5 @@
+package ticTacToeGameClass;
+
+public enum Player {
+	Empty, Cross, Nought
+}
